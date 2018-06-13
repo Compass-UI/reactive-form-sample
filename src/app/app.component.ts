a48f0b78432d56f89f63978a855fa0d9bc7b9f78
David Shams <david.shams@gmail.com>
2018-06-13 08:32:57 -0500

    WORKING: complete REACTIVE FORM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  searchForm;
+
+  submit(form){
+    console.log(form);
+  }
+  ngOnInit(){
+    let fromCity = new FormControl();
+    let toCity = new FormControl();
+    this.searchForm = new FormGroup({
+      fromCity: fromCity,
+      toCity: toCity
+    })
+  }
 }
