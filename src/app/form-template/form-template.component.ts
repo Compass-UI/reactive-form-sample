8dd92f3d00277991eb307cf64fd09410ec369dd8
David Shams <david.shams@gmail.com>
2018-06-15 11:51:40 -0500

    sample data population

--- src/app/form-template/form-template.component.ts
+++ src/app/form-template/form-template.component.ts
@@ -13,11 +13,24 @@ export class FormTemplateComponent implements OnInit {
   city: string = '';
   state: string = '';
   country: string = '';
-  imageUrl: string = 'https://images.pexels.com/photos/190589/pexels-photo-190589.jpeg?auto=compress&cs=tinysrgb&h=350';
+  imageUrl: string = '';
+
+  data: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.data = {
+      sine: 'sine',
+      agent: 'agent',
+      password: 'passx',
+      street: '1 Yonge',
+      city: 'Toronto',
+      state: 'ON',
+      country: 'Canada',
+      imageUrl: 'https://images.pexels.com/photos/190589/pexels-photo-190589.jpeg?auto=compress&cs=tinysrgb&h=350'
+
+    }
     
   }
   login(form){
@@ -25,6 +38,16 @@ export class FormTemplateComponent implements OnInit {
     // console.log(form);
     console.log(form.address);
     console.log(`User: ${form.agent} Agent: ${form.agent} pass: ${form.password}`);
+  }
+  sampleData(){
+    this.sine = this.data.sine;
+    this.agent = this.data.agent;
+    this.password = this.data.password;
+    this.street = this.data.street;
+    this.city = this.data.city;
+    this.state = this.data.state;
+    this.country = this.data.country;
+    this.imageUrl= this.data.imageUrl;
   }
 
 }
